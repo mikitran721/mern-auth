d0ed8556628c974bbc6c80a8abb27e1100ac376d
MikiTran <mikitran721@gmail.com>
2023-10-07 22:00:19 +0700

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//using dotenv
+dotenv.config();
+
+// ket noi csdl
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Loi ket noi MongoDB: ", err);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(3300, () => {
+  console.log("Server listening on port 3300");
 });
