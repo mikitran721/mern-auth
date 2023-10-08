2c04349b95bf2e418b267c7dec11099bd2eacade
MikiTran <mikitran721@gmail.com>
2023-10-08 19:59:55 +0700

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,52 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // function handleChange
+  const handleChange = (e) => {
+    if (error) setError(false);
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  // function handleSubmit
+  const handleSubmit = async (e) => {
+    // su dung dc endpoint nay do da set trong vite.config
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+
+      if (!data.success) {
+        setError(true);
+        return true;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h2 className="text-3xl text-center font-semibold my-7">Sign Up</h2>
-      <form className="flex flex-col gap-4 ">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <input
+          onChange={handleChange}
           type="text"
           placeholder="Username"
           id="username"
@@ -13,6 +54,7 @@ export default function SignUp() {
         />
 
         <input
+          onChange={handleChange}
           type="email"
           placeholder="Email"
           id="email"
@@ -20,14 +62,18 @@ export default function SignUp() {
         />
 
         <input
+          onChange={handleChange}
           type="password"
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
         />
 
-        <button className="bg-slate-700 text-white p-3 rounded-lg  uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg  uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -36,6 +82,7 @@ export default function SignUp() {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
     </div>
   );
 }
